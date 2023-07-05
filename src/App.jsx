f7220209e8cf520d8290eac1333864ef98911612
HASANUL BANNA C P <cphasanulbanna@gmail.com>
2023-07-05 12:38:37 +0530

    form styling completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,27 @@
 import React, { useState } from "react";
+
+//forms
 import BasicForm from "./components/basic-form/BasicForm";
 import AdvancedForm from "./components/advanced-form/AdvancedForm";
 
 function App() {
     const [view, setView] = useState("BASIC");
     return (
-        <div className="App">
-            <h3 onClick={() => setView("BASIC")}>Basic</h3>
-            <h3 onClick={() => setView("ADVANCED")}>Advanced</h3>
+        <div className="app">
+            <div className="options">
+                <h3
+                    className={view === "BASIC" && "active"}
+                    onClick={() => setView("BASIC")}
+                >
+                    Basic
+                </h3>
+                <h3
+                    className={view === "ADVANCED" && "active"}
+                    onClick={() => setView("ADVANCED")}
+                >
+                    Advanced
+                </h3>
+            </div>
 
             <div className="form-container">
                 {view === "BASIC" ? <BasicForm /> : <AdvancedForm />}
--- src/components/basic-form/BasicForm.jsx
+++ src/components/basic-form/BasicForm.jsx
@@ -1,9 +1,63 @@
 import React from "react";
 
 import "./basic-form.scss";
+import { useFormik } from "formik";
 
 function BasicForm() {
-    return <div>BasicForm</div>;
+    const formik = useFormik({
+        initialValues: {
+            email: "",
+        },
+    });
+    return (
+        <form autoComplete="off">
+            <label htmlFor="email">Email</label>
+            <input
+                type="email"
+                placeholder="Enter your email"
+                id="email"
+                value={formik.values.email}
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+            />
+
+            <label htmlFor="age">Age</label>
+            <input
+                type="number"
+                placeholder="Enter your age"
+                id="age"
+                value={formik.values.age}
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+            />
+
+            <label htmlFor="password">Password</label>
+            <input
+                type="password"
+                placeholder="Enter your password"
+                id="password"
+                value={formik.values.password}
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+            />
+
+            <label htmlFor="confirmPassword">Confirm Password</label>
+            <input
+                type="password"
+                placeholder="Confirm password"
+                id="confirmPassword"
+                value={formik.values.confirmPassword}
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+            />
+            <button
+                className="button"
+                type="submit"
+            >
+                Submit
+            </button>
+        </form>
+    );
 }
 
 export default BasicForm;
